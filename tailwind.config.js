5fd601254433d7d2440303bc083483eda3321a08
Thorsoll <william.cesto14@gmail.com>
2026-05-03 22:17:47 +0200

    ajout tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Vérifie bien que tes fichiers sont dans /src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
